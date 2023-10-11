4060f001d5a5a5be7af506a41bb3094b5f581509
najmul islam <najmulislam519@gmail.com>
2023-10-11 10:43:39 +0600

    Add password hashing, isValidPassword, genrateToken
    - remove avatar field in schema
    - add role field in schema
    - password hashing move in user model
    - method isValidPassword move in user model
    - method genrateToken move in user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,63 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
+      require: true,
+      unique: true,
     },
     email: {
       type: String,
+      require: true,
+      unique: true,
     },
     password: {
       type: String,
+      require: true,
     },
-    avatar: {
+    role: {
       type: String,
+      enum: ["admin", "moderator", "user"],
+      default: "user",
     },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+
+      if (this.email === process.env.ADMIN_EMAIL) {
+        this.role = "admin";
+      }
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
