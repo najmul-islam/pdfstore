b118042c2e04685c9b12b564eeb02037b54040f0
najmul islam <najmulislam519@gmail.com>
2023-10-13 12:29:28 +0600

    Add express-fileupload
    - add express-fileupload models for handling files
    - remove multer
    - add path module
    - change db to database
    - other stup

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,35 @@
+const path = require("path");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-
-const connectDB = require("./configs/db");
+const fileUpload = require("express-fileupload");
+const database = require("./configs/database");
 const errorHandler = require("./middlewares/errorMiddleware");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "./temp"),
+  })
+);
 
 // routes
 app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/books", require("./routes/bookRoute"));
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
 // error handler
 app.use(errorHandler);
 
+// port
 const port = process.env.PORT || 5000;
+
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await database(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (err) {
     console.log(err);
