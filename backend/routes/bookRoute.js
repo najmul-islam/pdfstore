ddb1815f71e37833ae4df5b702cd5326c9d00b75
najmul islam <najmulislam519@gmail.com>
2023-10-21 11:36:21 +0600

    Add route modify code
    - add bookUploader middleware
    - change getSingleBook controller
    - modify code base

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -2,24 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllBook,
-  getSignleBook,
+  getSingleBook,
   createBook,
   updateBook,
   deleteBook,
 } = require("../controllers/bookController");
 
-const upload = require("../middlewares/uploadPdfMiddleware");
-const protect = require("../middlewares/authMiddleware");
+const { bookUploader } = require("../middlewares/bookMiddleware");
+const { isUser } = require("../middlewares/authMiddleware");
 
-router.get("/", getAllBook);
-router.get("/:id", getSignleBook);
-router.post(
-  "/",
-  protect,
-  upload.single([{ name: "book", maxCount: 1 }]),
-  createBook
-);
-router.put("/:id", protect, updateBook);
-router.delete("/:id", protect, deleteBook);
+router.route("/").get(getAllBook).post(isUser, bookUploader, createBook);
+router
+  .route("/:id")
+  .get(getSingleBook)
+  .put(isUser, updateBook)
+  .delete(isUser, deleteBook);
 
 module.exports = router;
