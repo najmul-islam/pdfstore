39e383bdbc25dedcc9b2d118fe5464eec0e1c9a7
najmul islam <najmulislam519@gmail.com>
2024-06-05 10:52:35 +0600

    add book components

--- frontend/src/components/book/Book.jsx
+++ frontend/src/components/book/Book.jsx
@@ -1,27 +1,5 @@
-import { Container, Row, Col } from "react-bootstrap";
-import styled from "styled-components";
-
 const Book = ({ book }) => {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h4>Name: {book.name}</h4>
-          <h5>Year: {book.year}</h5>
-          <h6>Writer: {book.writer}</h6>
-          <hr />
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <h1>Book </h1>;
 };
 
 export default Book;
-
-const BookCard = styled.div`
-  background-color: #fff;
-  height: 100px;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  display: flex;
-`;
---
+++ frontend/src/components/book/CreateBook.jsx
@@ -0,0 +1,9 @@
+import { useFormik } from "formik";
+
+const CreateBook = () => {
+  const formik = useFormik({
+    initialValues: {},
+  });
+  return <div>CreateBook</div>;
+};
+export default CreateBook;
