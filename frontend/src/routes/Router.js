bad8fe755813c06ddd3a5247faf7918d4ed117e5
najmul islam <najmulislam519@gmail.com>
2024-06-05 10:47:20 +0600

    router file for all route

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -13,6 +13,12 @@ import DrivePage from "../pages/DrivePage";
 import LikePage from "../pages/LikePage";
 import AccountPage from "../pages/AccountPage";
 import UploadPage from "../pages/UploadPage";
+import AboutPage from "../pages/AboutPage";
+import ContactUs from "../pages/ContactUs";
+import TermsAndPrivacy from "../pages/TermsAndPrivacy";
+import CategoriesPage from "../pages/CategoriesPage";
+import CategoryPage from "../pages/CategoryPage";
+import Feedback from "../pages/Feedback";
 
 const Router = () => {
   return (
@@ -22,13 +28,19 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/category" element={<CategoriesPage />} />
+        <Route path="/category/:id" element={<CategoryPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/terms-and-privacy" element={<TermsAndPrivacy />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Route>
 
       {/* user route */}
       <Route path="/user/*" element={<UserLayout roles={["user"]} />}>
-        <Route path="drive" element={<DrivePage />} />
-        <Route path="like" element={<LikePage />} />
-        <Route path="account" element={<AccountPage />} />
+        <Route path="my-drive" element={<DrivePage />} />
+        <Route path="my-likes" element={<LikePage />} />
+        <Route path="my-account" element={<AccountPage />} />
         <Route path="upload" element={<UploadPage />} />
       </Route>
     </Routes>
