a2f96ad67a4d786009b0ef7d15294bf3e0e2f6bf
najmul islam <najmulislam519@gmail.com>
2024-06-05 10:45:58 +0600

    add theme reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
 import authReducer from "../features/auth/authSlice";
+import themeReducer from "../features/theme/themeSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    theme: themeReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   devTools: process.env.NODE_ENV !== "production",
